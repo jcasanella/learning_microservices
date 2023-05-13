58d5414293648a049c16b3ee0e517d0212ff73b7
Jordi Casanella <jordikiter@hotmail.es>
2023-05-13 23:43:03 +0100

    Fix Pug

--- src/express/views/ConfigView.ts
+++ src/express/views/ConfigView.ts
@@ -3,8 +3,8 @@ import { join } from 'path';
 
 export class ConfigView {
 	public static mount(_express: Application): Application {
-        console.log(join(__dirname, '..', '..', '..', 'views'));
-        _express.set('views', join(__dirname, '..', '..', '..', 'views'));
+        console.log(join(__dirname));
+        _express.set('views', join(__dirname));
         _express.set('view engine', 'pug');
 
         return _express;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import dotenv from 'dotenv';
+
 import Server from './express';
-
-dotenv.config();
-
-// const app: Express = express();
-const port = process.env.PORT;
 
 const app = new Server();
 app.init();
