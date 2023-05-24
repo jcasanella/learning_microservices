7cc8df70518a410431ac08866f00eab52e354677
Jordi Casanella <jordikiter@hotmail.es>
2023-05-25 00:20:06 +0100

    Changes in migration datasource

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,18 +1,28 @@
 import { DataSource } from 'typeorm'
+import dotenv from 'dotenv';
+import { join } from 'path';
 
-export const AppDataSource = new DataSource({
+dotenv.config({ path: join(__dirname, '../../.env') });
+
+export const appDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'example',
+    host: process.env.HOST_DB,
+    port: Number.parseInt(process.env.PORT_DB !== undefined ? process.env.PORT_DB : "5432"),
+    username: process.env.USERNAME_DB,
+    password: process.env.PASSWORD_DB,
     database: 'micro_videos',
     entities: ['dist/**/*.entity.js'],
     logging: true,
     synchronize: false,
     migrationsRun: false,
     migrations: ['dist/**/migrations/*.js']
-})
+});
 
-AppDataSource.initialize().then(async () => { console.log('Connected') });
+appDataSource.initialize()
+    .then(() => { 
+        console.log('DataSource has been initialized successfully.'); 
+    })
+    .catch((err) => {
+        console.error('Error during DataSource initialization: ', err);
+    });
 
