4c43f092f7a5ff0b1cb60eebdf72fba696a969f2
Jordi Casanella <jordikiter@hotmail.es>
2023-08-06 23:52:07 +0100

    First version lockRow

--- src/express/database/MessageRepository.ts
+++ src/express/database/MessageRepository.ts
@@ -1,12 +1,33 @@
 import { DataSource, Repository } from "typeorm";
 import { Messages } from "../../entities/messages.entity";
+import { MessagesAggregate } from "../../entities/messages_aggregate.entity";
 
 export class MessageRepository extends Repository<Messages> {
     constructor(readonly dataSource: DataSource){
         super(Messages, dataSource.createEntityManager());
     }
 
-    async append(id: string): Promise<void> {
-        
+    async append(message: Messages): Promise<void> {
+        const { streamName, id } = message;
+
+        await this.dataSource.transaction(async transactionEntityManager => {
+            const lockMessagesAggregate = await transactionEntityManager
+                .createQueryBuilder(MessagesAggregate, 'messagesAggregate')
+                .where('messagesAggregates.streamName = :streamName', {streamName})
+                .andWhere('messagesAggregate.id =:id', {id})
+                .setLock('optimistic', 1)
+                .getOne();
+
+            let version = lockMessagesAggregate?.version || 0;
+            console.log(`Current version ${version} for ${streamName} ${id}`);
+            version += 1;
+
+            // Save row message
+            message.version = version;
+            this.save(message);
+
+            // Update row 
+            
+        });
     }
 }
